27f4d8f1a11074d659e70e29bf4ee490d40ce0ab
xcry2006 <63004246+xcry2006@users.noreply.github.com>
2024-02-22 04:41:00 +0000

    change base64 encode

--- Encode/Base64.go
+++ Encode/Base64.go
@@ -6,11 +6,12 @@ package Base64
 
 import (
 	"encoding/base64"
+	"fmt"
 )
 
-func Base64Encode[T any](data []T) string {
-	str := string(data)
-	encode := []byte(str)
-	result := base64.StdEncoding.EncodeToString(encode)
+func Base64Encode(data interface{}) string {
+	byteData := []byte(fmt.Sprintf("%v", data) + "\n")
+	encode := base64.StdEncoding.EncodeToString(byteData)
+	result := (encode + "\n")
 	return result
 }
